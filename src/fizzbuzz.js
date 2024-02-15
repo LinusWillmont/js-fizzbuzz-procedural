b30b005fa589c4939688690e0499d20811a920dd
Linus Willmont <linus.willmont@se.experis.com>
2024-02-15 14:44:23 +0100

    Done

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,10 +1,19 @@
 const answer = []
 
 // Write your code below this line
+for (let index = 1; index < 16; index++) {
+  if (index % 3 === 0 && index % 5 === 0) {
+    answer.push('FizzBuzz')
+  } else if (index % 3 === 0) {
+    answer.push('Fizz')
+  } else if (index % 5 === 0) {
+    answer.push('Buzz')
+  } else {
+    answer.push(index)
+  }
+}
 
-
-
-
+answer.forEach((i) => console.log(i))
 
 // Don't touch the code below this line, we'll cover it later
 module.exports = answer
